d212a5f1cdc96df6771f4a0cd016ab12cac0340b
banna12345bob <banna12345bob@outlook.com>
2024-07-30 22:54:20 +1000

    Started work on launcher

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'f5776625-9acd-4220-9772-0b40b5f52181'
 // SEE NOTE ABOVE.
 
 
