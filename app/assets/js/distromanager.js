a47bc37f280d2197e1a350ed139fe811656fce0e
banna12345bob <banna12345bob@outlook.com>
2024-08-03 15:15:08 +1000

    Change name and distro url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://www.idiotss.com/launcher/distro-final.json'
+exports.REMOTE_DISTRO_URL = 'https://www.idiotss.com/download/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
